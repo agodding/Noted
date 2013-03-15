0c254727102e1a9d2fda0a36c8c032f7127a4143
Allan <allan.godding@gmail.com>
2013-03-14 21:18:13 -0400

    Fixed Lint warnings

--- gen/com/agodding/noted/R.java
+++ gen/com/agodding/noted/R.java
@@ -11,13 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int menu_delete=0x7f020002;
-        public static final int menu_labels=0x7f020003;
-        public static final int menu_lock=0x7f020004;
-        public static final int menu_unlock=0x7f020005;
-        public static final int rounded_rect=0x7f020006;
+        public static final int icon=0x7f020000;
+        public static final int menu_delete=0x7f020001;
+        public static final int menu_labels=0x7f020002;
+        public static final int menu_lock=0x7f020003;
+        public static final int menu_unlock=0x7f020004;
+        public static final int rounded_rect=0x7f020005;
     }
     public static final class id {
         public static final int addButton=0x7f070006;
@@ -49,6 +48,7 @@ public final class R {
         public static final int note_menu=0x7f060002;
     }
     public static final class string {
+        public static final int add=0x7f050005;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
         public static final int search_hint=0x7f050002;
